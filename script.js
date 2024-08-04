cd083880b1408a993b8ed182ef3856502f1c3f5c
bgwolfe <bwolfe53144@yahoo.com>
2024-08-04 08:03:15 -0500

    Added mouseover event to add color to grid squares and added a clear button

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 function createBoxes(num) {
     const container = document.getElementById('container');
     for (let i = 0; i < num; i++) {
@@ -15,6 +10,30 @@ function createBoxes(num) {
             boxRow.appendChild(box);
         }
     }
+    let newColor = document.querySelectorAll(".box");
+    for (const color of newColor) {
+        color.addEventListener("mouseover", () => {
+        color.style.backgroundColor = "black";
+        });
+    }
 }
 
-createBoxes(10);
+let boxNumber = 16;
+createBoxes(boxNumber);
+
+function clearScreen() {
+    document.querySelectorAll('.box').forEach(e => e.remove());
+    document.querySelectorAll('.boxRow').forEach(e => e.remove());
+    createBoxes(boxNumber);
+}
+
+
+function changeNumber() {
+    boxNumber = parseInt(prompt("Input a number between 2-100"));
+    if (101 > boxNumber && boxNumber > 1) {
+        clearScreen();
+    }
+}
+
+
+
